4aa213a99a1e8d8c332e367e3355ee9d1bdfb2be
andiisultaan <andiisultaan@gmail.com>
2024-10-11 09:47:29 +0700

    C:/Program Files/Git/

--- client/pictionary/src/routers/router.jsx
+++ client/pictionary/src/routers/router.jsx
@@ -6,7 +6,7 @@ import GamePage from "../views/GamePage";
 import LobbyPage from "../views/LobbyPage";
 
 import { io } from "socket.io-client";
-const socket = io("https://pictionary.yubi.web.id/", {
+const socket = io("https://pictionary.yubi.web.id", {
   autoConnect: false,
 });
 
